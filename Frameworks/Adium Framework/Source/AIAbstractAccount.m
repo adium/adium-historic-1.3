432c4277c2e4dde1dcdb7a8f496f9890dca0a24f
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2008-08-29 14:56:57 +0000

    Merged [25047]: AIInterfaceControllerProtocol changes for toggling the user list properly

--- Frameworks/Adium Framework/Source/AIAbstractAccount.m
+++ Frameworks/Adium Framework/Source/AIAbstractAccount.m
@@ -525,19 +525,7 @@
 	}
 }
 
-/*!
- * @brief Sent by an account to itself to update its user icon
- *
- * Both NSImage and NSData forms are passed to prevent duplication of data; either or both may be used.
- *
- * The image should be resized as needed for the protocol.
- *
- * Subclasses MUST call super's implementation.
- *
- * @param image An NSImage of the user icon, or nil if no image.
- * @param originalData The original data which made the image, which may be in any NSImage-compatible format, or nil if no image.
- */
-- (void)setAccountUserImage:(NSImage *)image withData:(NSData *)originalData;
+- (void)setAccountUserImage:(NSImage *)image withData:(NSData *)originalData
 {
 	//Notify
 	[[adium contactController] listObjectAttributesChanged:self
